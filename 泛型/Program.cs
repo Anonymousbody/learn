8064518ef67b4118fbc9040b57a0d7a70f707e9b
Nemo <522397130@qq.com>
2018-06-09 17:38:01 +0800

    添加项目文件。

---
+++ 泛型/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 逻辑;
+
+namespace 泛型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] a = new double[5] { 5, 7, 9, 11, 15 };
+            Calculator calculator = new Calculator();
+            Calculator.Calc calc;
+            calc = new Calculator.Calc(calculator.PF);
+            double[] c=Calculator.Apply(ref a, calc);
+            foreach (double b in a)
+                Console.Write("{0} ", b);
+            foreach (double b in c)
+                Console.Write("{0} ", b);
+            Console.Read();
+        }
+    }
+}
---
+++ 逻辑/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 逻辑
+{
+    public class Calculator
+    {
+        public delegate double Calc(double x);
+        public double PF(double x)
+        {
+            return x * x;
+        }
+        public double CF(double x)
+        {
+            return x * x * x;
+        }
+        public static double[] Apply(ref double[] a, Calc f)
+        {
+            double[] result = new double[a.Length];
+            for (int i = 0; i < a.Length; i++)
+                result[i] = f(a[i]);
+            return result;
+        }
+
+
+
+    }
+}
